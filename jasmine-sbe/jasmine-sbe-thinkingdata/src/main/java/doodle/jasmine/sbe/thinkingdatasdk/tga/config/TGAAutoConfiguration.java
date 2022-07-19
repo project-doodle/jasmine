890d4623be531865ca1e931f45394fe07c56e078
石头 <tingyan.shen@gmail.com>
2022-07-19 22:59:10 +0800

    chore: 添加 @ThinkingDataAnalytics javasdk 集成模块

---
+++ jasmine-sbe/jasmine-sbe-thinkingdata/src/main/java/doodle/jasmine/sbe/thinkingdatasdk/tga/config/TGAAutoConfiguration.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package doodle.jasmine.sbe.thinkingdatasdk.tga.config;
+
+import static doodle.jasmine.sbe.thinkingdatasdk.tga.TGAConstants.PREFIX;
+
+import cn.thinkingdata.tga.javasdk.Consumer;
+import cn.thinkingdata.tga.javasdk.ThinkingDataAnalytics;
+import doodle.jasmine.sbe.thinkingdatasdk.tga.conditon.ConditionalOnConsumerBatch;
+import doodle.jasmine.sbe.thinkingdatasdk.tga.conditon.ConditionalOnConsumerDebug;
+import doodle.jasmine.sbe.thinkingdatasdk.tga.conditon.ConditionalOnConsumerLogging;
+import java.net.URISyntaxException;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration(proxyBeanMethods = false)
+@ConditionalOnBean(TGAMarkerConfiguration.Marker.class)
+@ConditionalOnClass(ThinkingDataAnalytics.class)
+@EnableConfigurationProperties(TGAProperties.class)
+@ConditionalOnProperty(prefix = PREFIX, name = "app-id")
+public class TGAAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  @ConditionalOnConsumerDebug
+  public Consumer tgaDebugConsumer(TGAProperties properties) throws URISyntaxException {
+    return new ThinkingDataAnalytics.DebugConsumer(
+        properties.getServerUri(), properties.getAppId(), properties.getDebug().isWriteData());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  @ConditionalOnConsumerLogging
+  public Consumer tgaLoggingConsumer(TGAProperties properties) {
+    return new ThinkingDataAnalytics.LoggerConsumer(properties.getLogging());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  @ConditionalOnConsumerBatch
+  public Consumer tgaBatchConsumer(TGAProperties properties) throws URISyntaxException {
+    return new ThinkingDataAnalytics.BatchConsumer(
+        properties.getServerUri(), properties.getAppId(), properties.getBatch());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public ThinkingDataAnalytics thinkingDataAnalytics(Consumer consumer, TGAProperties properties) {
+    return new ThinkingDataAnalytics(consumer, properties.isEnableUUID());
+  }
+}
