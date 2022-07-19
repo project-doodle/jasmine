ac0d988e5564c44a559ec6152ad9318442bd2e87
石头 <tingyan.shen@gmail.com>
2022-07-20 00:41:51 +0800

    chore: 添加 @ThinkingDataAnalytics javasdk plus 模块

---
+++ jasmine-sbe/jasmine-sbe-thinkingdata-plus/src/main/java/doodle/jasmine/sbe/thinkingdata/tga/TGAPlusTracker.java
@@ -0,0 +1,78 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package doodle.jasmine.sbe.thinkingdata.tga;
+
+import cn.thinkingdata.tga.javasdk.ThinkingDataAnalytics;
+import java.util.Map;
+import java.util.Objects;
+
+public final class TGAPlusTracker {
+  private final ThinkingDataAnalytics tga;
+
+  public TGAPlusTracker(ThinkingDataAnalytics tga) {
+    this.tga = Objects.requireNonNull(tga);
+  }
+
+  public void track(
+      Object opaque,
+      String accountId,
+      String distinctId,
+      String eventName,
+      Map<String, Object> properties) {
+    try (TGAPlusOpaqueContext ignored = new TGAPlusOpaqueContext(opaque)) {
+      this.tga.track(accountId, distinctId, eventName, properties);
+    } catch (Throwable ignored) {
+    }
+  }
+
+  public void track_first(
+      Object opaque,
+      String accountId,
+      String distinctId,
+      String eventName,
+      Map<String, Object> properties) {
+    try (TGAPlusOpaqueContext ignored = new TGAPlusOpaqueContext(opaque)) {
+      this.tga.track_first(accountId, distinctId, eventName, properties);
+    } catch (Throwable ignored) {
+    }
+  }
+
+  public void track_update(
+      Object opaque,
+      String accountId,
+      String distinctId,
+      String eventName,
+      String eventId,
+      Map<String, Object> properties) {
+    try (TGAPlusOpaqueContext ignored = new TGAPlusOpaqueContext(opaque)) {
+      this.tga.track_update(accountId, distinctId, eventName, eventId, properties);
+    } catch (Throwable ignored) {
+    }
+  }
+
+  public void track_override(
+      Object opaque,
+      String accountId,
+      String distinctId,
+      String eventName,
+      String eventId,
+      Map<String, Object> properties) {
+    try (TGAPlusOpaqueContext ignored = new TGAPlusOpaqueContext(opaque)) {
+      this.tga.track_overwrite(accountId, distinctId, eventName, eventId, properties);
+    } catch (Throwable ignored) {
+    }
+  }
+}
---
+++ jasmine-sbe/jasmine-sbe-thinkingdata-plus/src/main/java/doodle/jasmine/sbe/thinkingdata/tga/config/TGAPlusAutoConfiguration.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2022-present Doodle. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package doodle.jasmine.sbe.thinkingdata.tga.config;
+
+import cn.thinkingdata.tga.javasdk.DynamicSuperPropertiesTracker;
+import cn.thinkingdata.tga.javasdk.ThinkingDataAnalytics;
+import doodle.jasmine.sbe.thinkingdata.tga.TGAPlusDynamicSuperPropertiesTracker;
+import doodle.jasmine.sbe.thinkingdata.tga.TGAPlusOpaqueContext;
+import doodle.jasmine.sbe.thinkingdata.tga.TGAPlusTracker;
+import doodle.jasmine.sbe.thinkingdatasdk.tga.config.TGAAutoConfiguration;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootConfiguration(proxyBeanMethods = false)
+@AutoConfigureAfter(TGAAutoConfiguration.class)
+@ConditionalOnBean(ThinkingDataAnalytics.class)
+public class TGAPlusAutoConfiguration {
+
+  @Bean
+  @ConditionalOnMissingBean
+  public DynamicSuperPropertiesTracker taDynamicSuperPropertiesTracker(
+      ObjectProvider<TGAPlusDynamicSuperPropertiesTracker> trackerProvider) {
+    return () ->
+        trackerProvider
+            .getIfUnique(() -> new TGAPlusDynamicSuperPropertiesTracker() {})
+            .apply(TGAPlusOpaqueContext.current());
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public TGAPlusTracker tgaPlusTracker(ThinkingDataAnalytics tga) {
+    return new TGAPlusTracker(tga);
+  }
+}
